38d2bf0babdc4f6c968494a8aaaf32b9273ee5d7
bhargavk95 <bhargavk.2709@gmail.com>
2016-11-18 06:11:55 -0500

    Documents upload with secure download link with css

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,7 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+#Formatting DateTime to look like "20/01/2011 10:28PM" 
+Time::DATE_FORMATS[:default] = "%m/%d/%Y %l:%M%p"
+
