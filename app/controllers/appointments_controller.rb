247960cfb904efdff151268c4fd45ca8a6a6d3a5
Jacob Varghese <jacobv1992@gmail.com>
2016-11-23 17:45:41 -0500

    Finished most of payment

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -6,7 +6,9 @@ class AppointmentsController < ApplicationController
   	@received_appointments.each do |appointment|
   		tmp = Hash.new
   		user = User.find(appointment.initiator_id)
-  		tmp[:id] = appointment.id
+      tmp[:id] = appointment.id
+      tmp[:appointment_id] = appointment.id
+      tmp[:ip_address] = request.remote_ip
   		tmp[:first_name] = user.first_name
   		tmp[:last_name] = user.last_name
   		tmp[:datetime] = appointment.datetime
@@ -24,9 +26,24 @@ class AppointmentsController < ApplicationController
   def make_payment
   	@payment = Payment.new
   	respond_to do |format|
-  		puts "================"
-  		format.json { head :no_content }
-        format.js
-  	end	
+      apt = Appointment.find(params[:id])
+      paid = apt.validate_and_pay(params, @payment, request.remote_ip )
+        		
+      if (paid.include? "Error")
+        flash[:error] = paid
+        redirect_to(:action => "index")
+      else
+        apt = Appointment.find(params[:id])
+        if apt.payment
+          apt.paid = true
+          apt.save 
+          flash[:success] = "Payment sucessfully made"
+          redirect_to(:action => "index")
+        end
+      end  
+      format.json { head :no_content }
+      format.js 
+
+    end   
   end	
 end
--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,5 +1,6 @@
 class Appointment < ApplicationRecord
   require 'date'
+  require "active_merchant/billing/rails"
 
   belongs_to :initiator, class_name: "User"
 	belongs_to :receiver,   class_name: "User"
@@ -16,5 +17,51 @@ class Appointment < ApplicationRecord
 
     return true
   end	
+
+
+  def validate_and_pay(payment_params, payment, ip_address)
+    if (payment_params[:payment])
+      payment = Payment.new(payment_parameters(payment_params))
+      payment.ip_address = ip_address
+      payment.appointment_id = self.id
+      create_card(payment_params[:payment])
+      if @credit_card.valid?
+        response = GATEWAY.purchase(price*100, @credit_card, :ip => payment.ip_address)
+        if (response.success?)
+          payment.save
+          attributes[:paid] = true
+          puts "===-----"
+          self.update_attributes(attributes)
+          return "true"                  
+        else
+          return "Error processing the payment. #{response.message}"          
+        end  
+    
+      else
+        return "Error: credit card is not valid. #{@credit_card.errors.full_messages.join('. ')}"
+      end
+    end
+    return "true"
+  end
+
+  private
+
+  def create_card(params)
+
+    @credit_card = ActiveMerchant::Billing::CreditCard.new(
+    :brand               => "visa",
+    :number             => params[:card_number],
+    :verification_value => params[:card_verification],
+    :month              => params[:card_expires_on].split("-")[0],
+    :year               => params[:card_expires_on].split("-")[1],
+    :first_name         => params[:first_name],
+    :last_name          => params[:last_name]
+    )
+
+  end 
+
+  def payment_parameters(payment_params)
+    payment_params.require(:payment).permit( :first_name, :last_name, :card_expires_on)
+  end 
 
 end
