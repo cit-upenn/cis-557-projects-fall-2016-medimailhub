7bea0d4d4c23f3f2424b91a126104f888892c1e4
Jacob Varghese <jacobv1992@gmail.com>
2016-11-28 02:32:27 -0500

    Added capybara for payment and appointments

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -10,10 +10,12 @@ class Appointment < ApplicationRecord
   has_one :payment
 
   def create_appointment(parameters, initiator)
-  		
+  	puts parameters.inspect	
     if (parameters[:appointment])
       self.update(:initiator_id => initiator.id, :receiver_id => parameters[:id], :price => parameters[:appointment][:price], :currency => "USD", :datetime => DateTime.parse(parameters[:appointment][:datetime]))
     end	
+
+    puts self.inspect
 
     return true
   end	
