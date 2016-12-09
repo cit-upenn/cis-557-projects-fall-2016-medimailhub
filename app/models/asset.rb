6725812ff0960605d710bd4a9fa8b20daf63ed48
bhargavk95 <bhargavk.2709@gmail.com>
2016-12-09 14:21:51 -0500

    fixed attachment content type and delete fuction

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -4,7 +4,7 @@ class Asset < ApplicationRecord
 
 #set up "uploaded_file" field as attached_file (using Paperclip) 
   
-  has_attached_file :uploaded_file, styles: { medium: "300x300>", thumb: "100x100#" },
+  has_attached_file :uploaded_file, #styles: { medium: "300x300>", thumb: "100x100#" },
 
                     :url => "/assets/get/:id", 
                     :path => ":Rails_root/assets/:id/:basename.:extension"
@@ -14,12 +14,13 @@ class Asset < ApplicationRecord
 
   #Only presence validation not working, have to give a file type, why?
 
-  validates_attachment_content_type :uploaded_file, content_type: [/\Aimage\/.*\z/, "application/pdf","application/vnd.ms-excel",     
-             "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-             "application/msword", 
-             "application/vnd.openxmlformats-officedocument.wordprocessingml.document", 
-             "text/plain"]
-  
+  # validates_attachment_content_type :uploaded_file, content_type: [/\Aimage\/.*\z/, "application/pdf","application/vnd.ms-excel",     
+  #            "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+  #            "application/msword", 
+  #            "application/vnd.openxmlformats-officedocument.wordprocessingml.document", 
+  #            "text/plain"]
+
+  validates_attachment_content_type :uploaded_file, content_type:[]
 
   def file_name 
     uploaded_file_file_name 
