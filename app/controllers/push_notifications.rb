992507db3c81317164603716889d7504a3bd78e4
Jacob Varghese <jacobv1992@gmail.com>
2016-12-09 23:24:26 -0500

    Added round logo again

--- app/controllers/push_notifications.rb
+++ app/controllers/push_notifications.rb
@@ -25,10 +25,11 @@ class PushNotification
 	end	
 
 	def send(token)
+		
 		uri = URI.parse("https://fcm.googleapis.com/fcm/send")
 		request = Net::HTTP::Post.new(uri, initheader = {'Content-Type' =>'application/json'})
 		request["Authorization"] = "key=AAAA1VIAL8o:APA91bEabWzsi5ygiOoiUiMsWtRXYvalv7eXEIgtjXAj7Ymp3MbgDbSl4-xepzRIBNIFPabkv68OJL1bx-86x5E9LtUG5gi1GOcIPmi-S5cMBeueVguLmRBBA7mCdFpOJh2tMVWKkEfuipcEM-6T8OXJRN4cLPNylA"
-		request.body = "{\"to\":\"#{token}\",\"notification\":{\"title\":\"#{@title}\",\"body\":\"#{body}\",\"click_action\":\"#{click}\",\"icon\": \"https://medimailhub.herokuapp.com/logo_blue.png\"} \"priority:\"10}"
+		request.body = "{\"to\":\"#{token}\",\"notification\":{\"title\":\"#{@title}\",\"body\":\"#{body}\",\"click_action\":\"#{click}\",\"icon\": \"https://medimailhub.herokuapp.com/final_round.png\"} \"priority:\"10}"
 
 		response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == "https") do |http|
   		http.request(request)
